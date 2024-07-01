69e696f21acbd77adb744efa879afcd5b9445bbe
manmandh <mantm.22ite@vku.udn.vn>
2024-07-01 23:05:36 +0700

    #3 convert login/register #4

--- src/controllers/shoes.controller.ts
+++ src/controllers/shoes.controller.ts
@@ -1,7 +1,14 @@
 import { Controller } from "../resources/types/index";
+import { Service, View } from "../utils/common";
 
 class ShoesController implements Controller {
-  constructor() {}
+  view: View;
+  service: Service;
+
+  constructor(view: View, service: Service) {
+    this.view = view;
+    this.service = service;
+  }
 }
 
 export default ShoesController;
